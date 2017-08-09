b028c5c78bdfa593ff2d61dbb368fa5007869fe1
janztec-ama <andre.massow@janz.de>
2017-08-09 13:31:36 +0200

    Create empc-can-configbaudrate.sh

---
+++ scripts/empc-can-configbaudrate.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+if [ $EUID -ne 0 ]; then
+    echo "This script should be run as root." > /dev/stderr
+    exit 1
+fi
+
+
+CANS=$(/usr/janz/bin/ixconfig 2>&1 | grep iX-Module | grep CAN | wc -l)
+for (( can=0; can<$CANS; can++ ))
+do
+
+        BAUDRATE=$(whiptail --title "Configure CAN$can baudrate" --radiolist \
+        "What is the baudrate of can$can bus?" 15 60 8 \
+        "1000" "1000 kBit/s" OFF \
+        "500" "500 kBit/s" OFF \
+        "250" "250 kBit/s (default)" ON \
+        "125" "125 kBit/s" OFF \
+        "100" "100 kBit/s" OFF \
+        "50" "50 kBit/s" OFF \
+        "20" "20 kBit/s" OFF \
+        "10" "10 kBit/s" OFF 3>&1 1>&2 2>&3)
+
+        exitstatus=$?
+        if [ $exitstatus = 0 ]; then
+
+         while read -r line
+         do
+          [[ ! "$line" =~ "can$can" ]] && echo "$line"
+         done </etc/network/interfaces > /tmp/interfaces
+         mv /tmp/interfaces /etc/network/interfaces
+
+         echo "# can$can" >>/etc/network/interfaces
+         echo "allow-hotplug can$can" >>/etc/network/interfaces
+         echo "iface can$can inet manual" >>/etc/network/interfaces
+         echo -e "\tpre-up /sbin/ip link set can$can type can bitrate $BAUDRATE""000 triple-sampling on" >>/etc/network/interfaces
+         echo -e "\tup /sbin/ifconfig can$can txqueuelen 1000" >>/etc/network/interfaces
+         echo -e "\tup /sbin/ifconfig can$can up" >>/etc/network/interfaces
+         echo -e "\tdown /sbin/ifconfig can$can down" >>/etc/network/interfaces
+
+        fi
+
+
+done
