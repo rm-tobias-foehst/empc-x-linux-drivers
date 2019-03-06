88b53322d0d484a4a5ec37ccb92caefad8f63b14
janztec-ama <14121872+janztec-ama@users.noreply.github.com>
2019-03-06 09:53:16 +0100

    apply Janz Tec specific patches

--- build-kernel.sh
+++ build-kernel.sh
@@ -6,7 +6,6 @@ ERR='\033[0;31m'
 INFO='\033[0;32m'
 NC='\033[0m' # No Color
 
-KERNEL=$(uname -r)
 
 if [ $EUID -ne 0 ]; then
     echo -e "$ERR ERROR: This script should be run as root. $NC" 1>&2
@@ -31,15 +30,25 @@ rm -f /usr/src/linux-source-*.tar.xz
 DEBIAN_FRONTEND=noninteractive apt-get install -y linux-source build-essential libncurses5-dev fakeroot bc
 
 cd /usr/src/
-rm -rf kernel
-mkdir -p kernel
-cd kernel
+rm -rf KERNEL
 
+mkdir -p KERNEL
+cd KERNEL
+
+# Apply Janz Tec specific Kernel patches
+wget -nv https://github.com/janztec/empc-x-linux-drivers/raw/master/src/0006-serial.patch -O 0006-serial.patch
+patch -p2 < 0006-serial.patch
+
+# Extract Kernel
 tar -xaf /usr/src/linux-source-*.tar.xz
+# Use current Kernel configuration
 cp /boot/config-`uname -r` .config
 make olddefconfig
+
+# Compile using 2 CPUs
 make -j2
 
+# Install Kernel and modules
 make modules_install
 make install
 
